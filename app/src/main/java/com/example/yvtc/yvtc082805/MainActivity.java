057845c8b05d242dd14bc623ff5ffca9e27d9a8d
Shaojung <shaojung@gmail.com>
2017-08-28 14:09:33 +0800

    Fragment

--- app/src/main/java/com/example/yvtc/yvtc082805/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc082805/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.yvtc.yvtc082805;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btna, btnb;
+    FragmentTransaction ft;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btna = (Button) findViewById(R.id.button3);
+        btnb = (Button) findViewById(R.id.button4);
+        btna.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment, new FragmentA(), "f_a");
+                ft.commit();
+            }
+        });
+
+        btnb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.fragment, new FragmentB(), "f_b");
+                ft.commit();
+            }
+        });
     }
 }
